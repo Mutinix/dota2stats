39373ded49b2a0b16387b84e606ab0c6c363f0e5
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2013-08-06 11:43:19 -0400

    Adds player-team link.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130806145227) do
+ActiveRecord::Schema.define(:version => 20130806154009) do
 
   create_table "leagues", :force => true do |t|
     t.string   "name"
     t.text     "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "player_teams", :force => true do |t|
+    t.integer  "player_id"
+    t.integer  "team_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end

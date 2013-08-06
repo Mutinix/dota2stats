32e7f2157eeaa626b872bcc5432195a5eeb719db
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2013-08-06 15:52:53 -0400

    Fixes redirect loop after sign-in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 DotaNexus::Application.routes.draw do
-
   
   devise_for :users, :controllers => {
                         :omniauth_callbacks => "users/omniauth_callbacks" }
                         
   devise_scope :user do
-    root to: "devise/sessions#new"
     get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
     get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
@@ -15,5 +13,4 @@ DotaNexus::Application.routes.draw do
   resources :teams, only: [:show]
   
   get '/roster', to: 'teams#show', as: :roster
-  
 end

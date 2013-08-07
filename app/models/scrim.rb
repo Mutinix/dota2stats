e59a259ef9e0bb2db89c02055dbd5f344757e8c1
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2013-08-06 21:54:52 -0400

    Creates partials for requesting other teams to scrim.

--- app/models/scrim.rb
+++ app/models/scrim.rb
@@ -1,3 +1,6 @@
 class Scrim < ActiveRecord::Base
   attr_accessible :match_time, :team1_id, :team2_id
+  
+  belongs_to :requester, foreign_key: :team1_id, :class_name => "Team"
+  belongs_to :requestee, foreign_key: :team2_id, :class_name => "Team"
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,13 @@ class Team < ActiveRecord::Base
   
   has_many :player_team
   has_many :players, through: :player_team, source: :user
+  
+  has_many :requests, foreign_key: :team2_id, :class_name => "Scrim"
+  has_many :challenges, foreign_key: :team1_id, :class_name => "Scrim"
+
+  has_many :requesters, through: :requests, :class_name => "Team",
+  foreign_key: :team2_id, source: :requester
+  has_many :challenged_teams, through: :challenges, :class_name => "Team",
+  foreign_key: :team1_id, source: :requestee
+
 end

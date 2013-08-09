fc88201194fcf26258891f69445c14dd48008ea4
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2013-08-09 11:12:44 -0700

    Adds match-time selection to scrim requests.

--- app/controllers/scrims_controller.rb
+++ app/controllers/scrims_controller.rb
@@ -2,9 +2,13 @@ class ScrimsController < ApplicationController
   
   def create
     params["scrim"]["team1_id"] = current_user.team.id
-    @scrim = Scrim.new(params["scrim"])
-    @scrim.save
     
+    params["scrim"]["match_time"] = DateTime.parse("#{params['scrim']['date']} #{params['scrim']['time']}")
+    params["scrim"].delete("date")
+    params["scrim"].delete("time")
+    
+    @scrim = Scrim.new(params["scrim"])
+    @scrim.save    
     if request.xhr?
       render partial: "scrim", locals: {scrim: @scrim}
     else

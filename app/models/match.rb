a99d1e666cea1904830ad05cedeb90477a918bc6
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2013-10-12 16:14:09 +0530

    Allows newly added columns to be mass assigned.

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,19 +2,30 @@
 #
 # Table name: matches
 #
-#  id            :integer          not null, primary key
-#  radiant_win   :boolean
-#  duration      :integer
-#  game_mode     :integer
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  match_seq_num :integer
-#  start_time    :integer
-#  lobby_type    :integer
+#  id                      :integer          not null, primary key
+#  radiant_win             :boolean
+#  duration                :integer
+#  game_mode               :integer
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  match_seq_num           :integer
+#  start_time              :integer
+#  lobby_type              :integer
+#  tower_status_radiant    :integer
+#  tower_status_dire       :integer
+#  barracks_status_radiant :integer
+#  barracks_status_dire    :integer
+#  cluster                 :integer
+#  first_blood_time        :integer
+#  human_players           :integer
+#  positive_votes          :integer
+#  negative_votes          :integer
 #
 
 class Match < ActiveRecord::Base
-  attr_accessible :duration, :game_mode, :radiant_win, :match_seq_num, :start_time, :lobby_type
+  attr_accessible :duration, :game_mode, :radiant_win, :match_seq_num, :start_time, :lobby_type,
+                  :tower_status_radiant, :tower_status_dire, :barracks_status_radiant, :barracks_status_dire,
+                  :cluster, :first_blood_time, :human_players, :positive_votes, :negative_votes
   
   has_many :player_matches, dependent: :destroy
   has_many :players, through: :player_matches, source: :user

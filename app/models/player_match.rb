c1f25ceb65f51e3ffb82c45657a8be521cc2109f
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2013-10-19 22:36:54 +0530

    Adds fields in PlayerMatch that can be mass-assigned.

--- app/models/player_match.rb
+++ app/models/player_match.rb
@@ -32,7 +32,7 @@
 #
 
 class PlayerMatch < ActiveRecord::Base
-  attr_accessible :assists, :deaths, :denies, :gold, :gold_per_min, :gold_spent, :hero_id, :item_0, :item_1, :item_2, :item_3, :item_4, :item_5, :kills, :last_hits, :level, :match_id, :player_id, :player_slot, :xp_per_min
+  attr_accessible :assists, :deaths, :denies, :gold, :gold_per_min, :gold_spent, :hero_id, :item_0, :item_1, :item_2, :item_3, :item_4, :item_5, :kills, :last_hits, :level, :match_id, :player_id, :player_slot, :xp_per_min, :hero_damage, :tower_damage, :hero_healing, :leaver_status
   
   belongs_to :user, foreign_key: :player_id
   belongs_to :match
